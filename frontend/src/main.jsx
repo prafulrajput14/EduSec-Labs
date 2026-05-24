2b317d8eeb35b9e92ea868705c248887dd56457d
Praful Rajput <aryanrajput1393@gmail.com>
2026-05-24 22:25:23 +0530

    Set production backend API URL

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import axios from "axios";
 import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
+axios.defaults.baseURL = "https://edusec-labs.onrender.com";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 )
